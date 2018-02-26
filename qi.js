bd5b52c2276a9168f6a24c7154ffbecf527b1a03
unknown <maciek2304@gmail.com>
2018-02-26 22:24:43 +0100

    Added basic support of background-image style property.

--- qi.js
+++ qi.js
@@ -1,21 +1,21 @@
 function Qi(queries) {
 
-    /* defining inner variables and defaults */
+    /* Defining inner variables and defaults. */
     var self = this;
     this.queries = queries ? queries : { small: 0, medium: 640, large: 1024 };
     this.breakpoints = [];
 
-    /* creating collection of images that correspond with given settings */
+    /* Creating collection of tags that correspond with given settings. */
     (function () {
         var query = "";
         for (var q in self.queries) {
-            query += "img[qi-" + q + "],";
+            query += "[qi-" + q + "],";
             self.breakpoints.push(q);
         }
-        self.imgs = document.querySelectorAll(query.slice(0, -1));
+        self.registered = document.querySelectorAll(query.slice(0, -1));
     })();
 
-    /* function that returns query notation valid with given configuration */
+    /* Function that returns query notation valid with given configuration. */
     this.getQuery = function () {
         var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         for (var i = 0; i < self.breakpoints.length - 1; i++) {
@@ -26,29 +26,30 @@ function Qi(queries) {
         return self.breakpoints[i];
     }
 
-    /* function that resizes all images */
+    /* Function that changes sources of all registered elements. */
     this.resize = function () {
         var query = self.getQuery();
         if (query === self.currentQuery) { return }
         self.currentQuery = query;
-        for (var i = 0; i < self.imgs.length; i++) {
-            var img = self.imgs[i];
-            var changed = false;
+        for (var i = 0; i < self.registered.length; i++) {
+            var element = self.registered[i];
             for (var j = self.breakpoints.indexOf(query); j >= 0; j--) {
-                var src = img.getAttribute('qi-' + self.breakpoints[j]);
-                if (src) {
-                    img.setAttribute('src', src);
-                    changed = true;
-                    j = -1;
+                var src = element.getAttribute('qi-' + self.breakpoints[j]);
+                if (element.tagName === 'IMG') {
+                    if (src) {
+                        element.setAttribute('src', src);
+                    } else {
+                        element.removeAttribute('src');
+                    }
+                } else {
+                    element.style.backgroundImage = src ? 'url(' + src + ')' : null;
                 }
-            }
-            if (!changed) {
-                img.setAttribute('src', "");
+                j = -1;
             }
         }
     }
 
-    /* reviving functionality */
+    /* Reviving functionality. */
     this.resize();
     if (window.addEventListener) {
         window.addEventListener('resize', self.resize, false);
@@ -57,7 +58,7 @@ function Qi(queries) {
     }
 }
 
-/* prototyping indexOf function for array (to provide compatibility with IE8, of course) */
+/* Prototyping indexOf function for array (to provide compatibility with IE8, of course). */
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (element) {
         for (var i = 0; i < this.length; i++) {
